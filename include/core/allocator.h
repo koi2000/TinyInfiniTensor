08b0ad504e8a8e138557b175fbfef6ff84d1b947
koi2000 <koi20000@163.com>
2024-08-11 16:31:32 +0800

    add allocator

--- include/core/allocator.h
+++ include/core/allocator.h
@@ -9,51 +9,71 @@
 #include <unordered_set>
 
 namespace infini {
-  class Allocator
-  {
-  private:
-    Runtime runtime;
 
-    size_t used;
+struct FreeBlock {
+  size_t addr;
+  size_t size;
+};
 
-    size_t peak;
+struct MemPool {
+  void *ptr;
+  size_t size;
+  size_t used;
+  std::map<size_t, FreeBlock> freeBlocks;
+};
 
-    size_t alignment;
+class Allocator {
+private:
+  Runtime runtime;
 
-    // pointer to the memory actually allocated
-    void *ptr;
+  size_t used;
 
-    // =================================== 作业 ===================================
-    // TODO：可能需要设计一个数据结构来存储free block，以便于管理和合并
-    // HINT: 可以使用一个 map 来存储 free block，key 为 block 的起始/结尾地址，value 为 block 的大小
-    // =================================== 作业 ===================================
+  size_t peak;
 
-  public:
-    Allocator(Runtime runtime);
+  size_t alignment;
 
-    virtual ~Allocator();
+  // pointer to the memory actually allocated
+  void *ptr;
 
-    // function: simulate memory allocation
-    // arguments：
-    //     size: size of memory block to be allocated
-    // return: head address offset of the allocated memory block
-    size_t alloc(size_t size);
+  std::vector<MemPool> pools;
 
-    // function: simulate memory free
-    // arguments:
-    //     addr: head address offset of memory block to be free
-    //     size: size of memory block to be freed
-    void free(size_t addr, size_t size);
+  // =================================== 作业
+  // ===================================
+  // TODO：可能需要设计一个数据结构来存储free block，以便于管理和合并
+  // HINT: 可以使用一个 map 来存储 free block，key 为 block
+  // 的起始/结尾地址，value 为 block 的大小
+  // =================================== 作业
+  // ===================================
+  std::map<size_t, FreeBlock> freeBlocks;
 
-    // function: perform actual memory allocation
-    // return: pointer to the head address of the allocated memory
-    void *getPtr();
+public:
+  Allocator(Runtime runtime);
 
-    void info();
+  virtual ~Allocator();
 
-  private:
-    // function: memory alignment, rouned up
-    // return: size of the aligned memory block
-    size_t getAlignedSize(size_t size);
-  };
-}
+  // function: simulate memory allocation
+  // arguments：
+  //     size: size of memory block to be allocated
+  // return: head address offset of the allocated memory block
+  size_t alloc(size_t size);
+
+  // function: simulate memory free
+  // arguments:
+  //     addr: head address offset of memory block to be free
+  //     size: size of memory block to be freed
+  void free(size_t addr, size_t size);
+
+  // function: perform actual memory allocation
+  // return: pointer to the head address of the allocated memory
+  void *getPtr();
+
+  void info();
+
+private:
+  // function: memory alignment, rouned up
+  // return: size of the aligned memory block
+  size_t getAlignedSize(size_t size);
+
+  MemPool *createNewPool(size_t size);
+};
+} // namespace infini
